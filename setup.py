fab0f3760dcfac7cde973ee7cb3b44f6d42f8dd8
Matthew Evans <git@ml-evs.science>
2021-05-28 18:11:29 +0100

    Add yaml req

--- setup.py
+++ setup.py
@@ -51,7 +51,7 @@ setup(
         "Topic :: Database :: Front-Ends",
     ],
     python_requires=">=3.9",
-    install_requires=["optimade~=0.14", "uvicorn", "aiofiles"],
+    install_requires=["optimade~=0.14", "uvicorn", "aiofiles", "pyyaml==5.4.1"],
     extras_require={
         "dev": dev_deps,
         "docs": docs_deps,
