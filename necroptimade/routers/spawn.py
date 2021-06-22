553306dede6a287586bd22d79bc197b2fdc863c6
Matthew Evans <git@ml-evs.science>
2021-06-22 22:22:30 +0100

    Some docstrings

--- necroptimade/routers/spawn.py
+++ necroptimade/routers/spawn.py
@@ -8,6 +8,7 @@ from optimade.server.routers import info, links, structures, references
 from optimade.models import (
     LinksResponse,
     LinksResource,
+    LinksResourceAttributes,
     ResponseMeta,
 )
 from optimade.server.routers.utils import BASE_URL_PREFIXES
@@ -18,6 +19,19 @@ ACTIVE = set()
 
 
 def spawn_optimade_app(request, params) -> LinksResponse:
+    """Creates a new OPTIMADE API based on the data
+    found at the passed location. If the API already
+    exists at that base URL, do not do anything.
+
+    Parameters:
+        request: The request that triggered the spawn.
+        params: The query parameters.
+
+    Returns:
+        A JSON:API links response pointing to the base URL
+            of the new OPTIMADE API.
+
+    """
 
     loc = getattr(params, "loc", None)
     if not loc:
@@ -39,18 +53,24 @@ def spawn_optimade_app(request, params) -> LinksResponse:
         create_routes(app_prefix)
         ACTIVE.add(app_prefix)
 
-    link = LinksResource(
-        id=app_prefix,
-        attributes=dict(
-            name="NecrOPTIMADE instance",
-            base_url=get_base_url(request.url) + app_prefix,
-            link_type="child",
-            homepage="https://necroptimade.herokuapp.com",
-            description=description,
-            aggregate="no",
-            no_aggregate_reason="This is an emphemeral NecrOPTIMADE instance.",
-        ),
+    link_attributes = LinksResourceAttributes(
+        name="NecrOPTIMADE instance",
+        base_url=get_base_url(request.url) + app_prefix,
+        link_type="child",
+        homepage="https://necroptimade.herokuapp.com",
+        description=description,
+        aggregate="no",
+        no_aggregate_reason="This is an emphemeral NecrOPTIMADE instance.",
     )
+
+    link = LinksResource(
+        id=app_prefix.strip("/"), type="links", attributes=link_attributes
+    )
+
+    # Insert child link into index meta-db
+    link_attributes.id = link.id
+    link_attributes.type = link.type
+    links.links_coll.collection.insert_one(json.loads(link_attributes.json()))
 
     return LinksResponse(
         data=[link],
@@ -64,8 +84,13 @@ def spawn_optimade_app(request, params) -> LinksResponse:
     )
 
 
-def create_routes(app_prefix):
+def create_routes(app_prefix: str) -> None:
+    """Add info and entry endpoints for the given app prefix.
 
+    Parameters:
+        app_prefix: The prefix of the new OPTIMADE API.
+
+    """
     for router in (info.router, links.router, structures.router, references.router):
         for version in ("major", "minor", "patch"):
             APP.include_router(router, prefix=app_prefix + BASE_URL_PREFIXES[version])
@@ -73,7 +98,17 @@ def create_routes(app_prefix):
         APP.include_router(versions.router, prefix=app_prefix)
 
 
-def ingest_data(loc):
+def ingest_data(loc: str) -> None:
+    """Attempt to ingest the data at the passed loc
+    as data to serve with an OPTIMADE API. Currently,
+    this function only deals with structure data, and will
+    drop any existing structure data when starting a new
+    ingestion.
+
+    Parameters:
+        loc: A string representing either a URL or a path to a local file.
+
+    """
 
     structures.structures_coll.collection.drop()
 
