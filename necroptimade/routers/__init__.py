3c93aed2b6fdb32fa2c533ec6660834b139edeb2
Matthew Evans <git@ml-evs.science>
2021-06-22 16:28:00 +0100

    Enable hosting local files

--- necroptimade/routers/__init__.py
+++ necroptimade/routers/__init__.py
@@ -1,4 +1,12 @@
-from fastapi import APIRouter, Depends, Request
+from fastapi import APIRouter, Depends, Request, Query
+
+
+class QueryParams:
+    def __init__(
+        self, *, loc: str = Query("", description="The location of the data to serve.")
+    ):
+        self.loc = loc
+
 
 spawn_router = APIRouter()
 
@@ -6,7 +14,7 @@ spawn_router = APIRouter()
 @spawn_router.get(
     "/spawn",
 )
-def spawn_optimade_endpoint(request: Request, params=Depends()):
+def spawn_optimade_endpoint(request: Request, params: QueryParams = Depends()):
     from necroptimade.routers.spawn import spawn_optimade_app
 
     return spawn_optimade_app(request=request, params=params)
